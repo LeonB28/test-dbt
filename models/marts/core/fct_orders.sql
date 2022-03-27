fbd840c292fa923e427c7958a8c1974b3e95200a
Leon Bam <leon.bam@gmail.com>
2022-03-27 11:36:08 +0000

    add stripe

---
+++ models/marts/core/fct_orders.sql
@@ -0,0 +1,17 @@
+with orders as (
+
+    select * from {{ ref('stg_orders') }}
+
+),
+
+payments as (
+
+    select * from {{ ref('stg_payments') }}
+)
+
+select
+    order_id,
+    customer_id,
+    amount
+from orders 
+left join payments using (order_id)   
