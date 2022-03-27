3ac34cff0340736125b72317c672dc95b06d6159
Leon Bam <leon.bam@gmail.com>
2022-03-27 12:06:11 +0000

    add lifetime spent value

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,2 +1,8 @@
-select *
-from `dbt-tutorial.stripe.payments`
+select 
+    id as id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    amount as amount,
+    created as created_at
+from `dbt-tutorial.stripe.payment`
